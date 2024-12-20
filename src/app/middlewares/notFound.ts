27087322057c72be7a892cf605511f65d70bd38c
aushamim <au.shamim0@gmail.com>
2024-12-21 03:07:55 +0600

    [Fix] Not Found

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 import { handleError } from "../utils/error";
 
 const notFound: RequestHandler = (req, res) => {
-  handleError(res, 404, "API route not found!", null);
+  handleError(res, 404, "API route not found!", {});
 };
 
 export default notFound;
