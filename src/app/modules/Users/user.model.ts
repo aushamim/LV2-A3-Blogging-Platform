d085d47e1c4ca434544ed95a168b98c7da26b83a
aushamim <au.shamim0@gmail.com>
2024-12-21 21:18:41 +0600

    [Fix] Not getting hashed password

--- src/app/modules/Users/user.model.ts
+++ src/app/modules/Users/user.model.ts
@@ -8,7 +8,7 @@ const UserSchema = new Schema<UserInterface>(
   {
     name      : { type: String, required: [true, "Name is required"], trim: true },
     email     : { type: String, required: [true, "Email is required"], trim: true, unique: true },
-    password  : { type: String, required: [true, "Password is required"], select: 0 },
+    password  : { type: String, required: [true, "Password is required"] },
     role      : { type: String, enum: ["admin", "user"], default: "user" },
     isBlocked : { type: Boolean, default: false },
   },
